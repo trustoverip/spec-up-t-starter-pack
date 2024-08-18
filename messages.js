2b3b45d81af95c5890cacfd864dadc8bf13a1425
kor <kor@dwarshuis.com>
2024-08-18 13:56:55 +0200

    Improve formatting

--- messages.js
+++ messages.js
@@ -1,12 +1,19 @@
 // Message shown when setup is complete. Array with two strings.
 const setupCompleteMessage = [
+
     `Spec-Up-T setup complete.\n\n
-     Now cd into the directory that you just created and run "npm install" to install dependencies:\n\ncd `,
-    `\n\nnpm install`];
+Now cd into the directory that you just created and run "npm install" to install dependencies:\n\n
+cd `,
+
+    `\n\nnpm install`
+
+];
 
 // Message shown when directory already exists. Array with two strings.
 const errorDirExistsMessage = [
+
     `The name you chose, ”`,
+
     `”, already exists. Exiting...`
 
 ];
