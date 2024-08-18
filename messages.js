46bd2af80ebaa82ee1a56356578291343418a5fd
kor <kor@dwarshuis.com>
2024-08-18 12:35:45 +0200

    To array, strings split in two

--- messages.js
+++ messages.js
@@ -1,5 +1,16 @@
-const setupCompleteMessage = `Spec-Up-T setup complete.\n\nNow cd into the directory that you just created and run "npm install" to install dependencies:\n\ncd ${dirName}\n\nnpm install`;
+// Message shown when setup is complete. Array with two strings.
+const setupCompleteMessage = [
+    `Spec-Up-T setup complete.\n\n
+     Now cd into the directory that you just created and run "npm install" to install dependencies:\n\ncd `,
+    `\n\nnpm install`];
+
+// Message shown when directory already exists. Array with two strings.
+const errorDirExistsMessage = [
+    `The name you chose, ”`,
+    `”, already exists. Exiting...`
+
+];
 
 module.exports = {
-    setupCompleteMessage
+    setupCompleteMessage, errorDirExistsMessage
 };
