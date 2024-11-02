dde1d5d6a07ce3ecb98c37948deac9b5c42e0ce4
kor <kor@dwarshuis.com>
2024-11-02 10:18:34 +0100

    Changes needed for the new install script

--- spec-up-t-starterpack/postinstall-message.js
+++ spec-up-t-starterpack/postinstall-message.js
@@ -1,2 +1,11 @@
+// load path module
+const path = require('path');
+// get current directory name
+const dirName = path.basename(process.cwd());
+
 // postinstall-message.js
-console.log('\n\n\n   Type the following and press ENTER: npm run menu\n\n\n');
+console.log(`\n\n\n============`);
+console.info(`\n   Type the following and press ENTER: cd ${dirName}\n`);
+console.log(`and then…`);
+console.log(`\n   Type the following and press ENTER: npm run menu\n`);
+console.log(`============\n\n\n`);
