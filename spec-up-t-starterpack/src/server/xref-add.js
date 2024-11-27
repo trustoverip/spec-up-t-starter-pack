4cbf079592fea4cea5444868e17a48f14a68f260
Kor Dwarshuis <proximus@fastmail.com>
2024-11-27 16:49:02 +0100

    Remove old JSDoc

--- spec-up-t-starterpack/src/server/xref-add.js
+++ spec-up-t-starterpack/src/server/xref-add.js
@@ -1,26 +1,3 @@
-/**
- * @file remove-xref.js
- * @description This script removes a cross-reference entry from an external specification file using the 
- * `spec-up-t` library. It takes two arguments: the term to be removed and the external 
- *  specification file where the removal should take place.
- * 
- *  This script is an intermediary between the `spec-up-t` library and the command line, to avoid issues
- *  with escaping quotes in the command line.
- *
- * @usage
- *   node remove-xref.js <term> <externalSpec>
- *   - <term>: The term to remove from the external specification.
- *   - <externalSpec>: The path to the external specification file where the cross-reference should be removed.
- *
- * @example
- *   node remove-xref.js "Aal" "test-1"
- *
- * @requires spec-up-t/src/get-xrefs-data.js
- * @function addXref
- */
-
-
-
 // // Version A: Get the term and external specification from the command line arguments
 // const args = process.argv.slice(2);
 // const term = args[0];
