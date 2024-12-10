b2633e36355a994ffe06a2adcd03d13eb4406ee7
Kor Dwarshuis <proximus@fastmail.com>
2024-12-10 18:49:25 +0100

    Not used

--- spec-up-t-starterpack/src/server/xref-update-all.js
+++ spec-up-t-starterpack/src/server/xref-update-all.js
@@ -1,5 +1,5 @@
 require('dotenv').config();
 
-const updateXrefs = require('spec-up-t/src/get-xtrefs-data.js').updateXTrefs;
+const updateXTrefs = require('spec-up-t/src/get-xtrefs-data.js').updateXTrefs;
 
-updateXrefs(process.env.GITHUB_API_TOKEN, false);
+updateXTrefs(process.env.GITHUB_API_TOKEN, false);
