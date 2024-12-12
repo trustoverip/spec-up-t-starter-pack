2ec8054657b02fe54d25b205ca63f32bc5b88c86
Kor Dwarshuis <proximus@fastmail.com>
2024-12-12 16:30:21 +0100

    Make directory name reflect actual dir name

--- spec-up-t-starterpack/postinstall-message.js
+++ spec-up-t-starterpack/postinstall-message.js
@@ -1,10 +1,15 @@
+// load path module
+const path = require('path');
+// get current directory name
+const dirName = path.basename(process.cwd());
+
 process.nextTick(() => {
     console.log(`
 ============
 
 Read more about Spec-Up-T on https://trustoverip.github.io/spec-up-t-website/
 
-Type the following and press ENTER: cd korkorkor
+Type the following and press ENTER: cd ${dirName}\n
 
 and then…
 
