fbc040641566211620d1d11ab2ea611081371780
Kor Dwarshuis <proximus@fastmail.com>
2024-12-15 16:14:22 +0100

    Rename xref to xtref

--- spec-up-t-starterpack/src/server/xref-update-all.js
+++ spec-up-t-starterpack/src/server/xref-update-all.js
@@ -1,5 +1,5 @@
 require('dotenv').config();
 
-const updateXrefs = require('spec-up-t/src/get-xrefs-data.js').updateXrefs;
+const updateXTrefs = require('spec-up-t/src/get-xtrefs-data.js').updateXTrefs;
 
-updateXrefs(process.env.GITHUB_API_TOKEN, false);
+updateXTrefs(process.env.GITHUB_API_TOKEN, false);
