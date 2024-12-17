f5de636bd3ad9f9edb1bd04dee3bf292fa96633c
Kor Dwarshuis <proximus@fastmail.com>
2024-12-17 15:18:00 +0100

    More robust handling of working dir

--- start.js
+++ start.js
@@ -1,10 +1,18 @@
 const { execSync } = require('child_process');
+const path = require('path');
 
 // Print the current working directory
 console.log('Current working directory:', process.cwd());
 
 // Print environment variables
 console.log('Environment variables:', process.env);
+
+// Change to the correct directory if needed
+const correctDir = path.resolve(__dirname, '..');
+if (process.cwd() !== correctDir) {
+    console.log(`Changing working directory to ${correctDir}`);
+    process.chdir(correctDir);
+}
 
 // Install dependencies
 console.log('Installing dependencies...');
