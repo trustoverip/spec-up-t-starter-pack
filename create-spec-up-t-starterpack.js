eecf00a88261bcf1c39d0818e2c86256b539c302
Kor Dwarshuis <proximus@fastmail.com>
2025-02-09 19:56:52 +0100

    Takes boilerplate from spec-up-t repo

--- create-spec-up-t-starterpack.js
+++ create-spec-up-t-starterpack.js
@@ -7,21 +7,28 @@ const { setupCompleteMessage, errorDirExistsMessage } = require('./messages.js')
 
 // Main setup function
 async function setupSpecPack(dirName) {
-    const sourceDir = path.join(__dirname, 'spec-up-t-boilerplate');
+    const packageSpecUpT = path.join(__dirname, 'package.spec-up-t.json');
     const targetDir = path.join(process.cwd(), dirName);
 
     try {
+
+        /* ****************
+            Install Git
+           **************** */
+
         if (fs.existsSync(targetDir)) {
             console.error(`${errorDirExistsMessage[0]}${dirName}${errorDirExistsMessage[1]}`);
             process.exit(1);
         }
+                
+        // Create targetDir if it doesn't exist
+        if (!fs.existsSync(targetDir)) {
+            await fs.mkdir(targetDir);
+            console.log(`Created directory: ${dirName}`);
+        }
 
-        // Copying starter pack files
-        await copyStarterPack(sourceDir, targetDir);
-        await renameGitignore(targetDir);
-        await updateReadme(dirName, targetDir);
-
-        console.log(`${setupCompleteMessage[0]}`);
+        // Copy package.spec-up-t.json to targetDir
+        await fs.copy(packageSpecUpT, path.join(targetDir, 'package.json'));
 
         // Run git init
         console.log(`Initialize git repository`);
@@ -39,33 +46,24 @@ async function setupSpecPack(dirName) {
         // execSync(`npm install`, { cwd: targetDir, stdio: 'inherit' });
         execSync(`npm install --silent`, { cwd: targetDir, stdio: 'inherit' });
 
+        console.log(`${setupCompleteMessage[0]}`);
+
+        /* ****************
+            Call spec-up-t install.js 
+           **************** */
+
+        const packageName = 'spec-up-t';
+        const basePath = path.join(targetDir);
+        const nodeModulesPath = path.join(basePath, 'node_modules');
+        const packagePath = path.join(nodeModulesPath, packageName);
+
+        require(path.join(packagePath, 'src', 'install-from-boilerplate', 'install.js'));
+
 
     } catch (err) {
         console.error('Error during setup:', err);
         process.exit(1);
     }
-}
-
-// Helper function to copy files
-async function copyStarterPack(source, target) {
-    await fs.copy(source, target);
-}
-
-// Helper function to rename gitignore
-async function renameGitignore(targetDir) {
-    const gitignorePath = path.join(targetDir, 'gitignore');
-    const dotGitignorePath = path.join(targetDir, '.gitignore');
-    if (await fs.existsSync(gitignorePath)) {
-        await fs.rename(gitignorePath, dotGitignorePath);
-    }
-}
-
-// Helper function to update README
-async function updateReadme(dirName, targetDir) {
-    const readmeFile = path.join(targetDir, 'README.md');
-    const data = await fs.readFile(readmeFile, 'utf8');
-    const result = data.replace(/spec-up-t-boilerplate/g, dirName);
-    await fs.writeFile(readmeFile, result, 'utf8');
 }
 
 // Start setup process with provided directory name
