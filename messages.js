6276e748c29f3906f2ef7aca00e9fb501a006fb7
Kor Dwarshuis <proximus@fastmail.com>
2025-02-14 12:36:24 +0100

    Improve console messages

--- messages.js
+++ messages.js
@@ -1,12 +1,12 @@
 // Message shown when setup is complete. Array with two strings.
 const setupCompleteMessage = [
-    `\n Spec-Up-T file copy complete.\n\n`
+    `\n✅ file copy complete.\n\n`
 ];
 
 // Message shown when directory already exists. Array with two strings.
 const errorDirExistsMessage = [
 
-    `\nThe name you chose, ”`,
+    `\n❌ The name you chose, ”`,
 
     `”, already exists. Exiting...`
 
